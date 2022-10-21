b6512454b9384dea0aac54af1fc5453edade994c
WilliamJamieEvans <jamieevans752@yahoo.co.uk>
2022-10-21 16:29:25 +0100

    Added HTML, CSS and JS Demo

---
+++ JAVASCRIPT/JsDemo.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// NEW function button (best way using external file)
+function myNEWFunction(){
+    document.getElementById("football").innerHTML = "City will win the league";
+} 
+// end of function button // 
---
+++ JAVASCRIPT/Lists.js
@@ -0,0 +1,19 @@
+'use strict'     //makes you use correct punctuation and helps with security
+
+ 
+console.log(23*19);   // this is how you do a console log
+
+
+// if, else statment 
+var x = 1;
+if(x>3){
+    console.log("x is greater than 3");
+} 
+else if(x==3){
+    console.log("x is equal to 3")
+}
+else {
+   console.log("x is not greater than 3"); 
+} 
+// end of if, else if, else statment //
+
